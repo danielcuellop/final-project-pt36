8a50b855eb3528b4ff2863ad0d2c31c58f818464
danielcuellop <franciscocuello86@gmail.com>
2023-06-20 00:19:13 +0000

    Daniel 19/06

--- src/api/models.py
+++ src/api/models.py
@@ -4,17 +4,15 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=False, nullable=False)
-    last_name = db.Column(db.String(120), unique=False, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
     rut = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    rol =  db.Column(db.String(20), unique=False, nullable=False)
-    password = db.Column(db.String(200), unique=False, nullable=False)
-    
-
+    rol = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(8000), nullable=False)
+    muestras = db.relationship('Muestra', backref='user', lazy=True)
     def __repr__(self):
-        return '<User %r>' % self.username
-
+        return f'<User {self.name}>'
     def serialize(self):
         return {
             "id": self.id,
@@ -22,25 +20,23 @@ class User(db.Model):
             "last_name": self.last_name,
             "rut": self.rut,
             "email": self.email,
-            "rol": self.rol,
-            "password": self.password
-            # do not serialize the password, its a security breach
+            "rol": self.rol
         }
     
 class Muestra(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    project_name = db.Column(db.String(150), unique=False, nullable=False)
-    ubication = db.Column(db.String(120), unique=False, nullable=False)
-    ubication_image = db.Column(db.String(120), unique=False, nullable=False)
-    area = db.Column(db.String(80), unique=False, nullable=False)
-    specimen = db.Column(db.String(80), unique=False, nullable=False)
-    quality_specimen = db.Column(db.String(80), unique=False, nullable=False)
-    image_specimen= db.Column(db.String(80), unique=False, nullable=False)
-    aditional_coments = db.Column(db.String(80), unique=False, nullable=False)
-
+    project_name = db.Column(db.String(150), nullable=False)
+    ubication = db.Column(db.String(120), nullable=False)
+    ubication_image = db.Column(db.String(120), nullable=False)
+    area = db.Column(db.String(80), nullable=False)
+    specimen = db.Column(db.String(80), nullable=False)
+    quality_specimen = db.Column(db.String(80), nullable=False)
+    image_specimen = db.Column(db.String(80), nullable=False)
+    aditional_comments = db.Column(db.String(80), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     def serialize(self):
         return {
-            "id" : self.id,
+            "id": self.id,
             "project_name": self.project_name,
             "ubication": self.ubication,
             "ubication_image": self.ubication_image,
@@ -48,5 +44,5 @@ class Muestra(db.Model):
             "specimen": self.specimen,
             "quality_specimen": self.quality_specimen,
             "image_specimen": self.image_specimen,
-            "aditional_coments": self.aditional_comments
-    }
+            "aditional_comments": self.aditional_comments
+        }
